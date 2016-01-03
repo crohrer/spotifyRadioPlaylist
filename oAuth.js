d795ae7dc4cd945c00f569362e48662ccf266c93
Christoph Rohrer <c@chrisrohrer.de>
2016-01-03 01:36:04 +0100

    Problem with oAuth access token: http://stackoverflow.com/questions/34572108/error-only-valid-bearer-authentication-supported-when-requesting-access-token

--- oAuth.js
+++ oAuth.js
@@ -6,10 +6,12 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 var url = require('url') ;
+var querystring = require('querystring');
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 var server = http.createServer(handleRequest);
 var PORT = 8585;
 var REDIRECT_URI = 'http://localhost:'+PORT;
+var SCOPES = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private';
 
 function handleRequest(request, response){
     data = '';
@@ -18,34 +20,38 @@ function handleRequest(request, response){
     });
     request.on('end', function () {
         var queryObject = url.parse(request.url, true).query;
+        response.end(JSON.stringify(queryObject));
 
         if(queryObject.code){ // this is the authorization code
             getToken(queryObject.code);
             server.close();
         }
-        response.end(JSON.stringify(queryObject));
     });
 }
 
 function getToken(code){
     var idAndSecret = config.clientId+':'+config.clientSecret;
     var authString = 'Basic ' + new Buffer(idAndSecret).toString('base64');
+    var data = querystring.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: REDIRECT_URI
+    });
     var tokenReq = https.request({
         hostname: 'api.spotify.com',
-        path: '/api/token',
+        path: '/api/token?'+data,
         method: 'POST',
         headers: {
             'Authorization': authString
         }
     }, function(res){
+        res.on('data', function(chunk){
+            console.log(new Buffer(chunk).toString());
+        });
         console.log(res.statusCode, JSON.stringify(res.headers));
     });
 
-    tokenReq.end(JSON.stringify({
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: REDIRECT_URI
-    }));
+    tokenReq.end();
 }
 
 function writeSecret(secret){
@@ -58,7 +64,13 @@ function writeSecret(secret){
 module.exports = {
     authenticate: function(){
         server.listen(PORT, function(){
-            console.log('Please log in first: https://accounts.spotify.com/authorize?client_id='+config.clientId+'&response_type=code&redirect_uri='+REDIRECT_URI);
+            var query = querystring.stringify({
+                client_id: config.clientId,
+                response_type: 'code',
+                scope: SCOPES,
+                redirect_uri: REDIRECT_URI
+            });
+            console.log('Please log in first: https://accounts.spotify.com/authorize?'+query);
         });
     }
-}
+};
